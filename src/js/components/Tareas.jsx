1e2671be25e3d96ca84276a4ca4cb0f7bebef656
HeidyDB <heidydiazbarrios@gmail.com>
2025-05-01 17:08:10 +0000

    agregar y quitar tareas DONE!

--- src/js/components/Tareas.jsx
+++ src/js/components/Tareas.jsx
@@ -6,63 +6,55 @@ import { useState } from "react";
 const Tareas = () => {
 
 
-  const [tareaNueva, setTareaNueva] = useState(""); //  
-  const [tareas, setTareas] = useState(["estudiar", "trabajar"]); //este es el arreglo de tareas
-
+  const [tareaNueva, setTareaNueva] = useState(""); //  esta es cada tarea nueva que entra
+  const [tareas, setTareas] = useState([]); //esta es la lista de tareas
 
   const agregar = () => { // esta tarea se agrega al dar enter en el teclado
     if (tareaNueva !== "") {
-      setTareas([...tareas, tareaNueva]); // añado la tarea nueva al arreglo de tareas
-      setTareaNueva("");
+      setTareas([...tareas, tareaNueva]); // añado la tarea nueva a la lista de tareas
+      setTareaNueva(""); //limpio el input para la proxima entrada 
     }
   }
 
-
-  /*const quitar = () => { // esta tarea se quitara con el evento hover cdo de click a la X
-    let selecc = setTareas[0];
-    while (let i < setTareas.length) {
-      if (selecc === setTareas[i]) {
-        setTareas[i] = " ";
-      } else { i++ }
-    }
-  }   */
+  const quitar = (indiceEliminar) => { // esta tarea se quitara con el evento onClick a la X
+      setTareas(tareas.filter((_, index) => index !== indiceEliminar)); //se hace un nuevo arreglo
+      //sin  
+  }   
 
   return (
     <div>
 
       <div className="container s-flex align-items-center " role="alert" >
-       
-      <div>
-            <input type="text " className = "form-control mt-100px"
-              placeholder='What needs to be done?'
-              onChange={(e) => { setTareaNueva(e.target.value) }} 
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  agregar();
-                }}} />
 
-          </div>
-          <div>
-            <ul className="list-group mt-3 w-100">
-
-              {tareas.map((value, index, array) => ( 
-                <li key={index} className="list-group-item 
+        <div> {/* introducir un valor (una tarea) */}
+          <input type="text " className="form-control mt-100px"
+            placeholder='What needs to be done?'
+            value={tareaNueva} 
+            onChange={(e) => { setTareaNueva(e.target.value) }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                agregar();
+              }
+            }} />
+        </div>
+        <div>
+          <ul className="list-group mt-3 w-100">
+            {/* recorro el arreglo devolviendo el valor en cada posicion */}
+            {tareas.map((value, index) => (
+              <li key={index} className="list-group-item 
                 d-flex justify-content-between align-items-center">
                 {value}
-                <button type="button" className="btn-close me-2 m-auto" aria-label="Close" 
-                onClick={() => eliminar(index)}></button>
-
-                </li>
-              ))}  { /*  cuando es codigo java script lo ponemos entre llaves */ }
-            </ul>
-          </div>
-          <div className="">{tareas.length}Item left</div>
-
-          
+                <button type="button" className="btn-close me-2 m-auto" aria-label="Close"
+                  onClick={() => quitar(index)}></button>
+              </li>
+            ))}  { /*  cuando es codigo java script lo ponemos entre llaves */}
+          </ul>
         </div>
-        </div>
-
-      )
+        <hr className="my-2 border border-ligth" /> 
+        <div className="">{tareas.length}   Item left</div>
+     </div>
+    </div>
+  )
 }
 
-      export default Tareas;
+export default Tareas;
